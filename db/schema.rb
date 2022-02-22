208dcf7df23510c5a93e5eac64ced325886a7fe2
Hugo Kubiak <hkubiak01@icloud.com>
2022-02-22 15:23:54 +0100

    Bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_151357) do
+ActiveRecord::Schema.define(version: 2022_02_22_122441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.string "status"
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id", null: false
+    t.bigint "moto_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["moto_id"], name: "index_bookings_on_moto_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "motos", force: :cascade do |t|
     t.string "name"
@@ -43,5 +55,7 @@ ActiveRecord::Schema.define(version: 2022_02_21_151357) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "motos"
+  add_foreign_key "bookings", "users"
   add_foreign_key "motos", "users"
 end
