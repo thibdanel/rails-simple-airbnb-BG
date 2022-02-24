3d500236be4a8555dea024162e3f0ec89ddf0011
Thibault Danel <thibdanel@hotmail.fr>
2022-02-24 13:26:32 +0100

    ok accept

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -18,12 +18,26 @@ class BookingsController < ApplicationController
     @moto = Moto.find(params[:moto_id])
     @booking.moto = @moto
     @booking.user_id = current_user.id
-    # @booking.status = "pending"
+    @booking.status = "pending"
     if @booking.save!
       redirect_to profile_path
     else
       render :new
     end
+  end
+
+  def accept
+    @booking = Booking.find(params[:id])
+    @booking.status = "accepted"
+    @booking.save!
+    redirect_to profile_path
+  end
+
+  def decline
+    @booking = Booking.find(params[:id])
+    @booking.status = "declined"
+    @booking.save!
+    redirect_to profile_paths
   end
 
   def destroy
